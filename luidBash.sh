3a4147f262677cbe5171448767ab737b732bf0f5
luid <eng.limaluis@gmail.com>
2024-03-06 17:49:45 -0300

    creating a script to install everthing I need

---
+++ luidBash.sh
@@ -0,0 +1,35 @@
+#   !usr/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+
+#git
+sudo apt-get install git-all
+cp dotfiles/.gitconfig ~/.gitconfig
+
+git clone https://github.com/Luidooo/dotfiles.git
+
+
+#vim
+sudo apt install vim -y
+cp dotfiles/.vimrc ~/.vimrc
+source ~/.vimrc
+
+#google
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt -f install
+
+#zoxide
+sudo apt install zoxide
+
+#fzf
+sudo apt install fzf
+
+#zsh
+sudo apt install zsh -y
+cp dotfiles/.zshrc ~/.zshrc
+source ~/.zshrc
+
+
