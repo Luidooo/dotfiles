495d9506f466f2266557a0d2a76be5c6da15bf7f
luid <eng.limaluis@gmail.com>
2024-05-29 10:59:43 -0300

    c

--- luidware.sh
+++ luidware.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-$trash=trash
-
 function packageStatus() {
 	if [ $? -eq 0 ]; then
-		echo "$package successfully installed" >>packagesInstalles
+		echo "$package - successfully installed" >>logs/packagesInstalled
 	else
-		echo "$package not successfully installed" >>packagesInstalles
+		echo "$package - not successfully installed" >>logs/packagesInstalled
 	fi
 }
 
 function downloadaPackage() {
-	sudo apt install $package -y >>a.log
+	sudo apt install $package -y >>logs/$package.log
 	packageStatus
 }
 
-sudo apt update -y >trash
-sudo apt upgrade -y >trash
+if [ ! -d logs ]; then
+	mkdir -p logs
+fi
+
+sudo apt update -y >logs/Update.log
+sudo apt upgrade -y >logs/Upgrade.log
 
 while IFS= read -r package; do
 	downloadaPackage
